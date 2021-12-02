a887b82a4b0f66b4423b47f165c7895249c6dc81
IDF31 <ardek66@tutanota.com>
2021-12-02 20:28:19 +0200

    Add day 2

--- app/Day02.hs
+++ app/Day02.hs
@@ -1,8 +1,27 @@
 module Day02 (solution) where
+import Data.List (foldl')
 import qualified AOC
 
-part1 = const 0
-part2 = const 0
+data Direction a = Forward a | Down a | Up a
+
+str2dir :: String -> Direction Integer
+str2dir = f . words
+  where f (dir:n:_) = (case dir of
+                        "forward" -> Forward
+                        "up" -> Up
+                        "down" -> Down) $ read n
+
+part1 = uncurry (*) . foldr f (0, 0)
+  where f dir = \(h, d) -> case dir of
+                             Forward n -> (h + n, d)
+                             Up n -> (h, d - n)
+                             Down n -> (h, d + n)
+
+part2 = (\(a, b, _) -> a * b) . foldl' f (0, 0, 0)
+  where f (h, d, a) dir = case dir of
+                            Forward n -> (h + n, d + a * n, a)
+                            Up n -> (h, d, a - n)
+                            Down n -> (h, d, a + n)
 
 solution = AOC.solution part1 part2
-           $ readFile "inputs/day1.txt"
+           $ map str2dir <$> lines <$> readFile "inputs/day2.txt"
